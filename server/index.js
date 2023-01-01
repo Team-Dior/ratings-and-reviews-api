6eff02e4f4ed97665340f2e0da5a024774572a5d
Kyle Robers <kyl.rober@gmail.com>
2022-12-31 19:23:56 -0700

    initial server index file

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan  = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+// const path = require('path');
+// require('dotenv').config({path: path.join(__dirname, '../.env')});
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const router = require('./router.js');
+
+app.use(router);
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => {
+  console.log(`listening @ http://localhost:${PORT}`);
+});
