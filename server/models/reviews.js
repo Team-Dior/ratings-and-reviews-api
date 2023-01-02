32dd19430f594f312f0aa4cbb8d0dfd7126707ec
Kyle Robers <kyl.rober@gmail.com>
2023-01-01 22:51:38 -0700

    implemented initial DB query for get reviews

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -1,33 +1,63 @@
 const db = require('../db/index.js');
 
 function getReviews(callback) {
-  const queryString = 'SELECT * FROM reviews WHERE id = 1';
+  // const page = query.page || 1;
+  // const count = query.count || 5;
+  // const sort = query.sort || 'newest';
+  // const product_id = query.product_id || 797894;
+  const product_id = 797894;
+  const queryString = `SELECT * FROM reviews WHERE product_id = ${product_id} LIMIT 5 OFFSET 1`;
   db.query(queryString, (err, result) => {
-
-  })
+    if (err) {
+      console.log('error is ', err);
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
 }
 
 function getMeta(callback) {
   db.query(queryString, (err, result) => {
-
+    if (err) {
+      console.log('error is ', err);
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
   })
 }
 
 function postReview(callback) {
   db.query(queryString, (err, result) => {
-
+    if (err) {
+      console.log('error is ', err);
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
   })
 }
 
 function putHelpful(callback) {
   db.query(queryString, (err, result) => {
-
+    if (err) {
+      console.log('error is ', err);
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
   })
 }
 
 function putReport(callback) {
   db.query(queryString, (err, result) => {
-
+    if (err) {
+      console.log('error is ', err);
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
   })
 }
 
