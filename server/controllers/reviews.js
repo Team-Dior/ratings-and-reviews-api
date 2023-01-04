c0419d742ffab3b44819ae922ac911b82ed9c4b1
Kyle Robers <kyl.rober@gmail.com>
2023-01-03 21:27:27 -0700

    updated controllers to handle variable data, query parameters, and params

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,20 +1,20 @@
 const models = require('../models/reviews.js');
 
 function getReviews(req, res) {
-  console.log('request is ', req);
-  models.getReviews(req.body, (err, result) => {
+  console.log('request is ', req.query);
+  models.getReviews(req.query, (err, result) => {
     if (err) {
-      console.log('error')
+      console.log('error', err)
       res.status(404).send();
     } else {
-      console.log('result in controllers ', result);
       res.status(200).send(result);
     }
   });
 }
 
 function getMeta(req, res) {
-  models.getMeta((err, result) => {
+  console.log('request is ', req.query);
+  models.getMeta(req.query, (err, result) => {
     if (err) {
       console.log('error', err);
     } else {
@@ -35,7 +35,8 @@ function postReview(req, res) {
 }
 
 function putHelpful(req, res) {
-  models.putHelpful((err, result) => {
+  console.log('request ', req.params);
+  models.putHelpful(req.params, (err, result) => {
     if (err) {
       console.log('error')
     } else {
@@ -45,7 +46,8 @@ function putHelpful(req, res) {
 }
 
 function putReport(req, res) {
-  models.putReport(data, (err, result) => {
+  console.log('request ', req.params);
+  models.putReport(req.params, (err, result) => {
     if (err) {
       console.log('error')
     } else {
