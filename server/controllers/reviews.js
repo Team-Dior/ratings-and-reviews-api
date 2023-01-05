d3b5bea2282a389d85ce57a80f4577fae397683d
Kyle Robers <kyl.rober@gmail.com>
2023-01-04 22:46:53 -0700

    messing with testData

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -23,10 +23,10 @@ function getMeta(req, res) {
 }
 
 function postReview(req, res) {
-  const testData = {product_id:953666, rating:2, summary:"big summary", body:"body text", recommend:true, name:"kyle", email:"kyl@email.com", photos:["urlplaceholder/review_5_photo_number_1.jpg", "urlplaceholder/review_5_photo_number_2.jpg"], characteristics:{"14": 5, "15":5} };
+  const testData = {product_id:5, rating:2, summary:"big summary", body:"body text", recommend:true, name:"kyle", email:"kyl@email.com", photos:["urlplaceholder/review_5_photo_number_1.jpg", "urlplaceholder/review_5_photo_number_2.jpg"], characteristics:{"14": 5, "15":5} };
   models.postReview(testData, (err, result) => {
     if (err) {
-      console.log('error')
+      console.log('error');
       res.status(404).send();
     } else {
       res.status(201).send();
@@ -37,7 +37,7 @@ function postReview(req, res) {
 function putHelpful(req, res) {
   models.putHelpful(req.params, (err, result) => {
     if (err) {
-      console.log('error')
+      console.log('error');
       res.status(404).send();
     } else {
       res.status(200).send();
@@ -48,7 +48,7 @@ function putHelpful(req, res) {
 function putReport(req, res) {
   models.putReport(req.params, (err, result) => {
     if (err) {
-      console.log('error')
+      console.log('error');
       res.status(404).send();
     } else {
       res.status(200).send();
