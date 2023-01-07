6c3b89a4a5e2048f4ad51b8d5e060b2224e5843d
Kyle Robers <kyl.rober@gmail.com>
2023-01-06 23:35:18 -0700

    updated routes for load balanced server with nginX

--- server/router.js
+++ server/router.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const controller = require('./controllers/reviews.js');
 
-router.get('/loaderio-e75786b9307ffbd8dc208dc579598a21/', (req, res) =>
-  res.send('loaderio-e75786b9307ffbd8dc208dc579598a21'));
+// router.get('/loaderio-e75786b9307ffbd8dc208dc579598a21/', (req, res) =>
+//   res.send('loaderio-e75786b9307ffbd8dc208dc579598a21'));
+
+router.get('/loaderio-6b4bc5cf9ca415c640f0a54b27b63cdb/', (req, res) =>
+  res.send('loaderio-6b4bc5cf9ca415c640f0a54b27b63cdb'));
 
 router.get('/reviews', controller.getReviews);
 router.get('/reviews/meta', controller.getMeta);
